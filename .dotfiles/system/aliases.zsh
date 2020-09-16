b140b7097a7191e24f1ef3317c8b3edb1563d227
Stephen Brown II <Stephen.Brown2@gmail.com>
2020-09-16 14:41:52 -0600

    Make ls aliases work better with exa

--- .dotfiles/system/aliases.zsh
+++ .dotfiles/system/aliases.zsh
@@ -1,11 +1,7 @@
 #!/bin/sh
-if [ "$(uname -s)" = "Darwin" ]; then
-	alias ls="ls -FG"
-else
-	alias ls="ls -F --color"
-fi
-alias l="ls -lAh"
-alias la="ls -A"
+
+alias l="ls -lah"
+alias la="ls -a"
 alias ll="ls -l"
 
 alias grep="grep --color=auto"
